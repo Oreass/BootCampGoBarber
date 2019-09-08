97d4111146c7cc586e8938ef7be6d6408fa39333
Oreas <ricnr.100@gmail.com>
2019-09-08 19:03:09 -0300

    Criação do UserContrller

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,17 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Ricardo',
-    email: 'ricnr.100@gmail.com',
-    password_hash: '1',
-  });
+routes.post('/users', UserController.store());
 
-  return res.json(user);
-});
+// routes.get('/', async (req, res) => {
+//    const user = await User.create({
+//    name: 'Ricardo',
+//    email: 'ricnr.100@gmail.com',
+//    password_hash: '1',
+//  });
+//  return res.json(user);
+// });
 
 export default routes;
