f1b191f54995d2141450426438c2db2f74dfd136
Oreas <ricnr.100@gmail.com>
2019-09-08 21:13:23 -0300

    security: Verify if user alredy exists before add.

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,8 +2,20 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    const user = await User.crete(req.body);
-    return res.json(user);
+    const userExists = await User.findone({ where: { email: req.body.email } });
+
+    if (userExists) {
+      return res.status(400).json({ error: 'User alredy exists' });
+    }
+
+    {id,name,email,provider} = await User.crete(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider
+    });
   }
 }
 
